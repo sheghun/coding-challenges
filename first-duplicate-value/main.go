8493e944dd2ab9fca98d3395e832c5b52b52749a
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2024-07-23 21:27:36 +0100

    done first duplicate

---
+++ first-duplicate-value/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func firstDuplicate(arr []int) int {
+	for _, num := range arr {
+		i := int(math.Abs(float64(num))) - 1
+		if arr[i] < 0 {
+			return int(math.Abs(float64(num))) 
+		} else {
+			arr[i] = arr[i] * -1
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(firstDuplicate([]int{2, 1, 5, 3, 3, 2, 4}))
+}
