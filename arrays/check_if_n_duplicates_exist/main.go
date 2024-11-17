1edfa7d0fa5e0edebedb42eebb45414846b3735e
Segun Oladiran <segunoladiran@seguns-m3-max.local>
2024-11-17 13:06:45 +0100

    done with if duplicates exist

---
+++ arrays/check_if_n_duplicates_exist/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func checkIfExist(arr []int) bool {
+	if len(arr) <= 1 {
+		return false
+	}
+
+	seen := make(map[int]int)
+
+  for i, val := range arr {
+    seen[val] = i
+  }
+
+	for i := 0; i < len(arr); i++ {
+		val := arr[i] * 2
+    if j, ok := seen[val]; ok && j != i {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+  fmt.Println(checkIfExist([]int{-2,0,10,-19,4,6,-8}))
+}
